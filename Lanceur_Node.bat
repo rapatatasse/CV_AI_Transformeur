a2f81eaf175ced4ae72a44f413a4f5cd04246c1a
MARTIN Hugo <h.martin@sio.local>
2026-05-19 11:25:27 +0200

    la fetcha de hoy numéro 2

---
+++ Lanceur_Node.bat
@@ -0,0 +1,5 @@
+# Script PowerShell pour lancer l'application Node.js
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
+
+cd "P:\2 Code\CV_AI_Transformeur\logiciels\node js"
+npm start
